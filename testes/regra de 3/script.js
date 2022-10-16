6fd2369df03a14cd3e2197cae490fd9725458bd5
lucas emanuel <lucascostar098@gmail.com>
2022-10-16 12:55:36 -0300

    new test

---
+++ testes/regra de 3/script.js
@@ -0,0 +1,17 @@
+
+    var a = document.getElementById('A')
+    var b = document.getElementById('B')
+    var c = document.getElementById('C')
+    var d = document.getElementById('D') 
+
+    
+   
+
+    function somar() {
+    
+    d.innerHTML = (Number.parseInt(c.value) * Number.parseInt(b.value)) / Number.parseInt(a.value)
+
+    
+    }
+
+  
